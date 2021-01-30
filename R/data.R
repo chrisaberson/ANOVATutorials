ddf5e87e6df667eb4b8b397a27e206f2f62d761c
chrisaberson <cla18@humboldt.edu>
2021-01-29 17:30:55 -0800

    Add files via upload

--- R/data.R
+++ R/data.R
@@ -10,3 +10,52 @@
 #' }
 
 "aa_framing"
+
+
+#' COVID 1 Factor
+#'
+#' A dataset examining interventions regarding COVID behaviors and beliefs
+#'
+#' @format A data frame with 389 rows and 4 variables:
+#' \describe{
+#'   \item{political}{political orientation}
+#'   \item{group}{Type of Intervention}
+#'   \item{Beliefs}{Beliefs about COVID}
+#'   \item{SD}{Social Distancing}
+#' }
+
+"missing"
+
+#' Missing data example
+#'
+#' A dataset examining job performance, satisfaction, well being, and research condition
+#'
+#' @format A data frame with 480 rows and 4 variables:
+#' \describe{
+#'   \item{Performance}{Job performance}
+#'   \item{Satifaction}{Job satifaction}
+#'   \item{WellBeing}{Well Being}
+#'   \item{Condition}{Condition}
+#'   \item{Sat_sq}{sq}
+#'   \item{Sat_log}{lg}
+#'   \item{Sat_inv}{inv}
+
+#' }
+
+"missing"
+
+"missing"
+
+#' Missing data example 2
+#'
+#' A dataset examining job performance, satisfaction, well being, and research condition
+#'
+#' @format A data frame with 480 rows and 4 variables
+#' \describe{
+#'   \item{Performance}{Job performance}
+#'   \item{Satifaction}{Job satifaction}
+#'   \item{WellBeing}{Well Being}
+#'   \item{Condition}{Condition}
+#' }
+
+"missing2"
